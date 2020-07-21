b8a28a2ddbf50bd84fbe8ac735d1c981c986c4c5
Evans Muzulu <emuzulu@oberlin.edu>
2020-07-21 09:39:33 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Evans Muzulu on 7/21/20.
+//  Copyright © 2020 Evans Muzulu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
